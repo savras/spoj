e174b843d5f029ecb8f27278a1fb280d1e17ffbe
Vincent Gim <vgim@sateva.com.au>
2017-04-16 13:54:54 +0800

    Added Sieve of Erestosthenes method

--- Q2PrimeGenerator/Q2PrimeGenerator/Source.cpp
+++ Q2PrimeGenerator/Q2PrimeGenerator/Source.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <list>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::list;
 
+ // Build primes for input range.
 void buildPrimes(vector<int>& primes, int size) {
 	primes.push_back(1);
 	primes.push_back(2);
@@ -47,7 +50,7 @@ int binarySearch(const vector<int>& primes, int start, int end, int target) {
 	}
 }
 
-int main() {
+void useCacheMethod() {
 	int t;
 	cin >> t;
 
@@ -68,5 +71,45 @@ int main() {
 			cout << primes[index] << endl;
 			index++;
 		}
+	}
+}
+
+void buildSieve(vector<int>& primes) {
+	// Init
+	int size = primes.size();
+	for (size_t i = 2; i < size; i++) {
+		if (!primes[i]) {
+			continue;
+		}
+
+		for (size_t p = i * i; p < size; p += i) {
+			primes[p] = false;
+		}
+	}
+}
+
+// https://www.khanacademy.org/computing/computer-science/cryptography/comp-number-theory/v/sieve-of-eratosthenes-prime-adventure-part-4
+void useSieveMethod() {
+	int t;
+	cin >> t;
+
+	int m, n;
+	const int size = 31623;	// sqrt(1000000000) + 1	(see video of example for n = 100. We still need to process for i == 11).
+	vector<int> primes(size, true);
+	buildSieve(primes);
+
+	for (size_t p = 0; p < t; p++) {
+		cin >> m >> n;
+
+		for (size_t i = m; i <= n; i++) {
+			if (primes[i]) {
+				cout << i << endl;
+			}
+		}
 	}	
+}
+
+int main() {
+	//useCacheMethod();
+	useSieveMethod();	
 }
