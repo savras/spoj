f598d7c0e5ee41f46de2d06d9d3e0b9215c7a32d
Vincent Gim <vgim@sateva.com.au>
2017-04-18 21:39:38 +0800

    Added proof and explanations

--- Q2PrimeGenerator/Q2PrimeGenerator/Source.cpp
+++ Q2PrimeGenerator/Q2PrimeGenerator/Source.cpp
@@ -10,18 +10,19 @@ using std::vector;
 using std::list;
 
 // https://www.khanacademy.org/computing/computer-science/cryptography/comp-number-theory/v/sieve-of-eratosthenes-prime-adventure-part-4
-void buildPrimes(vector<int>& primes) {
+void markAllPrimesInArray(vector<int>& primes) {
 	// Init
 	int size = primes.size();
 	primes[0] = 0;
 	primes[1] = 0; // 0 & 1 are not primes
 
 	for (size_t i = 2; i < size; i++) {
+		// We can encounter values of i that have already been marked as not prime
 		if (!primes[i]) {
 			continue;
 		}
 
-		// i * i is because we start from the square of i (see Khan Academy video)
+		// p = i * i because we start from the square of i (see Khan Academy video)
 		// http://stackoverflow.com/questions/5811151/why-do-we-check-up-to-the-square-root-of-a-prime-number-to-determine-if-it-is-pr
 		for (size_t p = i * i; p < size; p += i) {
 			primes[p] = false;
@@ -32,17 +33,30 @@ void buildPrimes(vector<int>& primes) {
 int getStartIndex(const int& p, const int& m) {
 	int result;
 	if (m > p) {
-		int firstDivisibleNumber = (m / p) * p;		
+		// Get the first number divisible by p where p <= m.
+		int firstNumberDivisibleByPBeforeM = (m / p) * p;
 
-		if (firstDivisibleNumber == m) {
-			result = firstDivisibleNumber - m;
+		if (firstNumberDivisibleByPBeforeM == m) {
+			// If the number is equal to m, we can start the Sieve from m, e.g. Sieve[m] == false;
+			result = firstNumberDivisibleByPBeforeM - m;
 		}
 		else {
-			result = firstDivisibleNumber + p - m;	// -m to map start of value, m, to index 0 of sieve array				
+			// Otherwise we add p to that number (so that it is >= m)
+			// We subtract m to map to the range of m to n in the Sieve array
+			// e.g. when p = 2, m = 99, firstNumberDivisibleByPBeforeM == 98.
+			// Therefore, result = 98 + 2 - 99 == 1, whereby 
+			// Sieve[1] represents the value 100 in the range from m to n.
+			result = firstNumberDivisibleByPBeforeM + p - m;	
 		}
 	}	
 	else {
-		result = (p * 2) - m;	// start from the next p index after p
+		// We can just start sieve from Sieve[2p - m].
+		// For all cases where p >= m, the first time we process p, p will be a prime number.
+		// E.g.: p == 5, m == 4, then 5 is a prime.
+		// Likewise, p == 71, m == any number less than p (i.e. 61), then p is a prime 
+		// E.g. 2: p == 61, m == 61, then 61 is a prime. 
+		// So, we can start marking Sieve[(p * 2) - m] is NOT A PRIME.
+		result = (p * 2) - m;
 	}
 
 	return result;	
@@ -57,12 +71,14 @@ void getPrimesForRange(const vector<int>& primes, const int& m, const int& n) {
 		sieve[0] = 0;
 	}
 
-	// Get primes starting from the smallest value divisible by p (from <= m ) until == n	
+	// Get primes starting from the smallest value divisible by p within range (where p <= m ) up until p == n
 	for (size_t i = 2; i < primes.size(); i++) {
 		if (primes[i]) {
 			int index = getStartIndex(i, m);
 			if (index > size) { break; }
-			for (size_t p = index; p < size; p += i) {	// p = i because we are not starting from integer 2
+
+			// Perform Sieve of Eratosthenes
+			for (size_t p = index; p < size; p += i) {
 				sieve[p] = false;
 			}
 		}		
@@ -83,7 +99,7 @@ int main() {
 	int m, n;
 	const int size = 31623;	// sqrt(1000000000) + 1	(see video of example for n = 100. We still need to process for i == 11).
 	vector<int> primes(size, true);
-	buildPrimes(primes);
+	markAllPrimesInArray(primes);
 
 	for (size_t p = 0; p < t; p++) {
 		cin >> m >> n;	
