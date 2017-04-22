2147c66406164694a8f2df56592a0ef11ae2cbb8
Vincent Gim <vgim@sateva.com.au>
2017-04-22 19:45:31 +0800

    WIP

--- Q5Palindrome/Q5Palindrome/Source.cpp
+++ Q5Palindrome/Q5Palindrome/Source.cpp
@@ -5,5 +5,7 @@ using std::cout;
 using std::endl;
 
 int main() {
-
+	// Most significant digit is tied to least significant digit.
+	// Therefore, we get the least significant digit starting from before (for odd) or equal to middle digit (for even)
+	// and increase it by one?
 }
