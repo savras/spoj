9adccc8ab5014e923816be416102f95e30534b41
Vincent Gim <vgim@sateva.com.au>
2017-04-23 20:58:13 +0800

    Updated logic

--- Q5Palindrome/Q5Palindrome/Source.cpp
+++ Q5Palindrome/Q5Palindrome/Source.cpp
@@ -6,22 +6,28 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::stoi;
 using std::ceil;
+using std::to_string;
 
 string getNextPalindrome(string& expr) {
 	int left, mid;
 	int size;
-	const int offset = 1;	
+	const int offset = 1;
 
 	size = expr.length();
-	mid = ceil(size / 2) - offset;	// Always pointing to mid - 1 regardless if expr has odd or even length.
-	left = mid;
+	mid = size / 2;
+	if (size % 2 == 0) {
+		mid--;
+	}
 
-	bool isAlreadyPalindrome = false;	// Handles the case where this algorithm doesn't work if the number given is already a palindrome.
+	left = mid;
+	
 
 	while (left >= 0) {
 		if (expr[left] != expr[size - offset - left]) {
 			left = mid;
+
 			while (expr[left] == '9') {
 				left--;
 			}
@@ -36,9 +42,11 @@ string getNextPalindrome(string& expr) {
 			for (size_t i = 0; i <= mid; i++) {
 				expr[size - offset - i] = expr[i];
 			}
+
+			break;
 		}
 		left--;
-	}
+	}	
 	return expr;
 }
 
@@ -48,6 +56,16 @@ int main() {
 	string expr;	
 	while (--t >= 0) {
 		cin >> expr;
-		cout << getNextPalindrome(expr) << endl;
+		int number = stoi(expr);
+
+		string nextPalindrome;
+		nextPalindrome = getNextPalindrome(expr);
+		
+		int result = stoi(nextPalindrome);
+		if (number == result) {	// This algorithm doesn't handle inputs that are already a palindrome.
+			number++;
+			nextPalindrome = getNextPalindrome(to_string(number));
+		}
+		cout << nextPalindrome << endl;
 	}
 }
