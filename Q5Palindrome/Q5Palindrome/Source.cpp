507e5ebfff1559e289b57c6293a0cf65c54ae038
Vincent Gim <vgim@sateva.com.au>
2017-04-23 19:33:19 +0800

    Initial solution

--- Q5Palindrome/Q5Palindrome/Source.cpp
+++ Q5Palindrome/Q5Palindrome/Source.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+using std::ceil;
 
 int main() {
-	// Most significant digit is tied to least significant digit.
-	// Therefore, we get the least significant digit starting from before (for odd) or equal to middle digit (for even)
-	// and increase it by one?
+	int t;
+	cin >> t;
+
+	string expr;
+	int left, mid;
+	int size;
+	const int offset = 1;
+
+	while (--t >= 0) {
+		cin >> expr;
+		size = expr.length();
+
+		mid = ceil(size / 2) - offset;	// Always pointing to mid - 1 regardless if expr has odd or even length.
+		left = mid;
+
+		while (left >= 0) {
+			if (expr[left] != expr[size - offset - left]) {
+				left = mid;
+				while (expr[left] == '9') {
+					left--;
+				}
+				expr[left]++;
+
+				// Set all digits between left to mid == 0
+				for (size_t i = left + 1; i <= mid; i++) {
+					expr[i] = '0';
+				}
+
+				// Set everything to the right side of mid to become a palindrome.
+				for (size_t i = 0; i <= mid; i++) {
+					expr[size - offset - i] = expr[i];
+				}
+			}
+
+			left--;
+		}
+		cout << expr << endl;;
+	}
 }
