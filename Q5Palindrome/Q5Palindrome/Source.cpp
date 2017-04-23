e0e45182fdd1cee9ddac0d276ea5ebfe0cabd1fd
Vincent Gim <vgim@sateva.com.au>
2017-04-23 20:14:08 +0800

    Moved logic into its own function

--- Q5Palindrome/Q5Palindrome/Source.cpp
+++ Q5Palindrome/Q5Palindrome/Source.cpp
@@ -8,43 +8,46 @@ using std::endl;
 using std::string;
 using std::ceil;
 
+string getNextPalindrome(string& expr) {
+	int left, mid;
+	int size;
+	const int offset = 1;	
+
+	size = expr.length();
+	mid = ceil(size / 2) - offset;	// Always pointing to mid - 1 regardless if expr has odd or even length.
+	left = mid;
+
+	bool isAlreadyPalindrome = false;	// Handles the case where this algorithm doesn't work if the number given is already a palindrome.
+
+	while (left >= 0) {
+		if (expr[left] != expr[size - offset - left]) {
+			left = mid;
+			while (expr[left] == '9') {
+				left--;
+			}
+			expr[left]++;
+
+			// Set all digits between left to mid == 0
+			for (size_t i = left + 1; i <= mid; i++) {
+				expr[i] = '0';
+			}
+
+			// Set everything to the right side of mid to become a palindrome.
+			for (size_t i = 0; i <= mid; i++) {
+				expr[size - offset - i] = expr[i];
+			}
+		}
+		left--;
+	}
+	return expr;
+}
+
 int main() {
 	int t;
 	cin >> t;
-
-	string expr;
-	int left, mid;
-	int size;
-	const int offset = 1;
-
+	string expr;	
 	while (--t >= 0) {
 		cin >> expr;
-		size = expr.length();
-
-		mid = ceil(size / 2) - offset;	// Always pointing to mid - 1 regardless if expr has odd or even length.
-		left = mid;
-
-		while (left >= 0) {
-			if (expr[left] != expr[size - offset - left]) {
-				left = mid;
-				while (expr[left] == '9') {
-					left--;
-				}
-				expr[left]++;
-
-				// Set all digits between left to mid == 0
-				for (size_t i = left + 1; i <= mid; i++) {
-					expr[i] = '0';
-				}
-
-				// Set everything to the right side of mid to become a palindrome.
-				for (size_t i = 0; i <= mid; i++) {
-					expr[size - offset - i] = expr[i];
-				}
-			}
-
-			left--;
-		}
-		cout << expr << endl;;
+		cout << getNextPalindrome(expr) << endl;
 	}
 }
