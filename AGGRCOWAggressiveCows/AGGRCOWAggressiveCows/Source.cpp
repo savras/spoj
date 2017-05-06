912d2a1d0dc150b3c821eb02f184300269fc53de
Vincent Gim <vgim@sateva.com.au>
2017-05-06 17:08:48 +0800

    Tidy up

--- AGGRCOWAggressiveCows/AGGRCOWAggressiveCows/Source.cpp
+++ AGGRCOWAggressiveCows/AGGRCOWAggressiveCows/Source.cpp
@@ -5,6 +5,7 @@
  * So, for each of given the stall values, calculate the distances from stalls[0], then see how many
  * cows you can fit into the stalls array with that distance.
  *
+ * Brute force: perform predicate() for each stall position, N, until you get cowCount == totalCow.
  * Hints: 
  *    https://www.quora.com/What-is-the-correct-approach-to-solve-the-SPOJ-problem-Aggressive-cow
  *    https://discuss.codechef.com/questions/77613/spoj-problem-aggrcow-aggressive-cows
@@ -20,6 +21,7 @@ using std::endl;
 using std::vector;
 using std::sort;
 
+//O(N), where you have as many cows as you have stalls, and mid is very small.
 bool predicate(const vector<int>& stalls, const int& mid, const int& totalCows) {
 	int cowCount = 1;	// Starting count. First cow is always on the left most of the search space.
 	int candidateDistance = stalls[mid] - stalls[0];		// Distance of stalls[mid] from 1st cow.
@@ -40,6 +42,7 @@ bool predicate(const vector<int>& stalls, const int& mid, const int& totalCows) 
 
 // We will get an array like this |Y|Y|Y|Y|N|N|N|.
 // What we want is the value in index 3, which is the rightmost Y.
+// O(log X), where X is the number of positions in the stalls.. isn't X == N? I don't understand the question.
 int binary_search(const vector<int>& stalls, int start, int end, const int& totalCows) {
 	while (start < end) {
 		const int handleRoundUp = 1;
@@ -63,6 +66,8 @@ int main() {
 	int t, n, c;
 	cin >> t;
 
+	vector<int> result;
+
 	vector<int> stalls;
 	while (--t >= 0) {
 		cin >> n >> c;
@@ -71,30 +76,38 @@ int main() {
 			cin >> value;
 			stalls.push_back(value);
 		}
-		sort(stalls.begin(), stalls.end());	
-		cout << binary_search(stalls, 0, stalls.size() - 1, c) << endl;
+		sort(stalls.begin(), stalls.end());		// O(N log N)
+		result.push_back(binary_search(stalls, 0, stalls.size() - 1, c));
 
 		stalls.clear();
 	}
+
+	for (size_t i = 0; i < result.size(); i++) {
+		cout << result[i] << endl;
+	}
 }
 
-//int binary_search_recursive(const vector<int>& stalls, const int& start, const int& end, const int& totalCows) {
-//	int minDistanceIndex = stalls[end];
-//
-//	if (start < end) {
-//		const int handleRoundUp = 1;
-//		// Handle |YES|NO| situation where we will call binary_search with mid = x and end = x + 1 all the time.
-//		// By doing this, we will always select the right side of the |YES|NO| as the mid instead of the left.
-//		int mid = start + (end - start + handleRoundUp) / 2;	
-//		bool pResult = predicate(stalls, mid, totalCows);
-//
-//		if (pResult) {
-//			minDistanceIndex = binary_search_recursive(stalls, mid, end, totalCows);
-//		}
-//		else {
-//			minDistanceIndex = binary_search_recursive(stalls, start, mid - 1, totalCows);
-//		}
-//	}	
-//
-//	return stalls[minDistanceIndex] - stalls[0];
-//}
+/*
+Sample input:
+6
+10 3
+1 2 9 8 4 4 8 9 2 1
+5 3
+1 2 8 4 9
+20 3
+9 8 7 10 6 5 4 3 2 1 19 18 17 16 15 14 13 12 11 20
+3 3
+0 1000000000 500000000
+20 4
+9 8 7 10 6 5 4 3 2 1 19 18 17 16 15 14 13 12 11 20
+20 5
+9 8 7 10 6 5 4 3 2 1 19 18 17 16 15 14 13 12 11 20
+
+Output:
+3
+3
+9
+500000000
+6
+4
+*/
