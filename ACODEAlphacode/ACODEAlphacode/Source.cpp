446f1e5e25dcd03fc1738505864bdb110890b895
Vincent Gim <vgim@sateva.com.au>
2017-05-10 23:33:55 +0800

    Initial solution

--- ACODEAlphacode/ACODEAlphacode/Source.cpp
+++ ACODEAlphacode/ACODEAlphacode/Source.cpp
@@ -9,30 +9,39 @@ using std::string;
 using std::vector;
 
 void main() {
-	int hasInput;
-	cin >> hasInput;
+	string input;
 
-	string code;	
-	while (hasInput) {
-		cin >> code;
-		int size = code.length();
-		vector<char> arr(size, 0);
+	cin >> input;
+	while (input != "0") {
+		int size = input.length();
+		vector<int> arr(size, 0);
 		arr[0] = 1;
 		string numCode = "";
 
 		// ToDo:: Validate that code has no 0's;		
 		for (size_t i = 1; i < size; i++) {
 			numCode = "";
-			numCode.push_back(code[i - 1]);
-			numCode.push_back(code[i]);
+			numCode.push_back(input[i - 1]);
+			numCode.push_back(input[i]);
 
 			int value = stoi(numCode);
 			if (value > 26) {
 				arr[i] = arr[i - 1];
 			}
 			else {
-				arr[i] = arr[i - 1] + arr[i - 2];
+				int previous = i - 1;
+				int prevPrevious = i - 2;
+
+				if (prevPrevious >= 0)
+				{
+					arr[i] = arr[i - 1] + arr[i - 2];
+				}
+				else {
+					arr[i] = arr[i - 1] + 1;
+				}
 			}
 		}
-	}
+		cout << arr[size - 1] << endl;
+		cin >> input;
+	} 
 }
