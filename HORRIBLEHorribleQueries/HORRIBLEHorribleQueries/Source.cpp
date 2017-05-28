42d2095ec2bdf6a6c1c9378976bcdf19f6908b24
Vincent Gim <vgim@sateva.com.au>
2017-05-28 23:37:53 +0800

    Added initial logic for solution using BIT

--- HORRIBLEHorribleQueries/HORRIBLEHorribleQueries/Source.cpp
+++ HORRIBLEHorribleQueries/HORRIBLEHorribleQueries/Source.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::vector;
+using std::string;
 
-// BIT implementation
+// BIT/Fenwick implementation
+int getNext(const int& index) {
+	return index & (index - 1);
+}
+
+int getParent(const int& index) {
+	return index + (~index);
+}
+
+long long getAt(const vector<int>& arr, const int& index) {
+	long long result = 0;
+	int parent = getParent(index);
+	while (parent > 0) {
+		result += arr[parent];
+	}
+
+	return result;
+}
+
+void update(vector<int>& arr, const int& index, const int& value) {
+	int next = getNext(index);
+	while (next < arr.size()) {
+		arr[next] += value;
+	}
+}
 
 // Segment tree implementation
 
@@ -14,10 +42,21 @@ int main() {
 
 	cin >> t;
 	while (t--) {
-		cin >> n >> c;
+		cin >> n;
+		while (n--) {
+			vector<int> arr(n);
+			cin >> c;
+			while (c--) {
+				cin >> code >> xi >> yi >> value;
+				if (code == 1) {
+					update(arr, xi, value);
+				}
+				else {
+
+				}
+			}
+		}
 	}
 
-
-		
 	return 0;
 }
