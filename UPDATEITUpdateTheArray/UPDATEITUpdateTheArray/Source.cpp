a04d15fa7d766e05a6a03728e9f2e0570870c079
Vincent Gim <vgim@sateva.com.au>
2017-05-31 00:40:28 +0800

    Solved UPDATEIT using BIT

--- UPDATEITUpdateTheArray/UPDATEITUpdateTheArray/Source.cpp
+++ UPDATEITUpdateTheArray/UPDATEITUpdateTheArray/Source.cpp
@@ -5,7 +5,38 @@ using std::cin;
 using std::vector;
 using std::ios_base;
 
-// Naive implementation.
+int getParent(const int& index) {
+	return index - (index & (-index));
+}
+
+int getNext(const int& index) {
+	return index + (index & (-index));
+}
+
+long long getAt(const int& index, vector<long long>& arr) {
+	int current = index;
+	long long result = 0;
+	do {
+		result += arr[current];
+		current = getParent(current);
+	} while (current > 0);
+
+	return result;
+}
+
+void update(vector<long long>& arr, const int& index, const int& value) {
+	int current = index;
+	do {
+		arr[current] += value;
+		current = getNext(current);
+	} while (current < arr.size());
+}
+
+void prebuildArray(vector<long long>& arr, const int& l, const int& r, const int& val) {
+	update(arr, l, val);
+	update(arr, r + 1 , -val);
+}
+
 int main() {
 	int t;
 	scanf("%d", &t);
@@ -15,18 +46,15 @@ int main() {
 
 	while (t--) {
 		scanf("%d%d", &n, &u);
-		vector<long long> arr(n);
-		while (u--) {
+		vector<long long> arr(n + 2);
+		while (u--) {			
 			scanf("%d%d%d", &l, &r, &val);
-
-			for (size_t i = l; i <= r; i++) {
-				arr[i] += val;
-			}
+			prebuildArray(arr, l + 1, r + 1, val);
 		}
 		scanf("%d", &q);
 		while (q--) {
-			scanf("%d", &p);
-			printf("%d\n", arr[p]);
+			scanf("%d", &p);			
+			printf("%lld\n", getAt(p + 1, arr));
 		}
 	}
 }
