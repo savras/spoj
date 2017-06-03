8119c60278e16626037504f888c71c59e2ba33f0
Vincent Gim <vgim@sateva.com.au>
2017-06-03 19:56:26 +0800

    Initial lazy propagation segment tree implementation - untested

--- HORRIBLEHorribleQueries/HORRIBLEHorribleQueries/Source.cpp
+++ HORRIBLEHorribleQueries/HORRIBLEHorribleQueries/Source.cpp
@@ -38,7 +38,47 @@ void update(vector<int>& arr, const int& index, const int& value) {
 }
 
 // Segment tree implementation
-long long getValue(vector<int>& st, const int& s, const int& e, const int& l, const int& r, const int& current) {
+long long update(vector<int>& arr, vector<int>& lazy, const int& s, const int& e, 
+	const int& l, const int& r, const int& value, const int& current) {
+
+	int leftChild = (2 * current) + 1;
+	int rightChild = (2 * current) + 2;
+
+	if (l < s  && r > e) {
+		return 0;
+	}
+
+	if (s >= l && e <= r) {
+		if (lazy[current] != 0) {
+			arr[current] += lazy[current];
+			lazy[current] = 0;
+
+			if (leftChild <= lazy.size() - 1)
+			{
+				lazy[leftChild] += value;
+			}
+			if (rightChild <= lazy.size() - 1)
+			{
+				lazy[rightChild] += value;
+			}
+		}
+		arr[current] += value;
+		return arr[current];
+	}
+
+	int mid = s + ((e - s) / 2);
+	long long leftVal = update(arr, lazy, s, mid, l, r, value, leftChild);
+	long long rightVal = update(arr, lazy, mid + 1, e, l, r, value, rightChild);
+	
+	if (lazy[current] != 0)
+	{
+		arr[current] += lazy[current];
+		lazy[current] = 0;		
+	}
+	return arr[current];
+}
+
+long long getValue(vector<int>& st, vector<int> lazy, const int& s, const int& e, const int& l, const int& r, const int& current) {
 	if (l <= s && r >= e) {
 		return st[current];
 	}
@@ -48,8 +88,8 @@ long long getValue(vector<int>& st, const int& s, const int& e, const int& l, co
 	}
 
 	int mid = s + (e - s) / 2;
-	long long leftVal = getValue(st, s, mid, l, r, (2 * current) + 1);
-	long long rightVal = getValue(st, mid + 1, e, l, r,  2 * current + 2);
+	long long leftVal = getValue(st, lazy, s, mid, l, r, (2 * current) + 1);
+	long long rightVal = getValue(st, lazy, mid + 1, e, l, r,  2 * current + 2);
 	return leftVal + rightVal;
 }
 
@@ -75,16 +115,15 @@ int main() {
 	cin >> t;
 	while (t--) {
 		cin >> n;
-		vector<int> arr(n + 1);
-
+		vector<int> arr(n);
+		vector<int> lazy(n);
+		
 		cin >> c;
 		while (c--) {
 			cin >> code;
 			if (code == 0) {
 				cin >> xi >> yi >> value;
-
-
-				//update(arr, xi, value);
+				update(arr, lazy, 0, n - 1, xi, yi, value, 0);
 			}
 			else {
 				cin >> xi >> yi;
