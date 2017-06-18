567ead48f3a0ee9dbed599572f0c906f0e84ec58
Vincent Gim <vgim@sateva.com.au>
2017-06-18 16:34:25 +0800

    UPDATEIT: Added prefix sum solution

--- UPDATEITUpdateTheArray/UPDATEITUpdateTheArray/Source.cpp
+++ UPDATEITUpdateTheArray/UPDATEITUpdateTheArray/Source.cpp
@@ -48,24 +48,62 @@ void prebuildArray(vector<long long>& arr, const int& l, const int& r, const int
 	update(arr, r + 1 , -val);
 }
 
-int main() {
-	int t;
-	scanf("%d", &t);
-
+void solveBIT(int& t) {
 	int n, u, l, r, val;
 	int q, p;
 
 	while (t--) {
 		scanf("%d%d", &n, &u);
+		// arr(n + 2) because one is for BIT index 0 placeholder and the other is for when 
+		// we do BIT[r + 1] -= sum; which will go out of bounds for BIT[n].
+		// Here, we use arr as our BIT.
 		vector<long long> arr(n + 2);
-		while (u--) {			
+		while (u--) {
 			scanf("%d%d%d", &l, &r, &val);
 			prebuildArray(arr, l + 1, r + 1, val);
 		}
 		scanf("%d", &q);
 		while (q--) {
-			scanf("%d", &p);			
+			scanf("%d", &p);
 			printf("%lld\n", getAt(p + 1, arr));
 		}
 	}
+}
+
+void buildPrefixSum(vector<long long>& ps, const vector<long long>& arr, const int& size) {
+	ps[0] = arr[0];
+	for (size_t i = 1; i < size; i++) {
+		ps[i] = arr[i] + ps[i - 1];
+	}
+}
+
+void solvePrefixSum(int& t) {
+	int n, u, l, r, val;
+	int q, p;
+
+	while (t--) {
+		scanf("%d%d", &n, &u);
+		vector<long long> arr(n + 1);
+		while (u--) {
+			scanf("%d%d%d", &l, &r, &val);
+			arr[l] += val;
+			arr[r + 1] -= val;
+		}
+
+		vector<long long> ps(n);
+		buildPrefixSum(ps, arr, n);
+		scanf("%d", &q);
+		while (q--) {
+			scanf("%d", &p);
+			printf("%lld\n", ps[p]);
+		}
+	}
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	
+	//solveBIT(t);
+	solvePrefixSum(t);
 }
