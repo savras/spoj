2730d90ea25339278d49b6b00dfe45eb911767a4
Vincent Gim <vgim@sateva.com.au>
2017-06-23 23:35:59 +0800

    PT07Y: Initial solution to detect cycle

--- 1436_PT07Y/PT07Y/PT07Y/Program.cs
+++ 1436_PT07Y/PT07Y/PT07Y/Program.cs
@@ -25,37 +25,45 @@ namespace PT07Y
                 var inSplit = inEdge.Split(' ');
                 var node = int.Parse(inSplit[0]);
                 var edge = int.Parse(inSplit[1]);
-                adj[node].Add(edge);
+                adj[node - 1].Add(edge - 1);
             }
 
             // DFS
             Solve(adj);
         }
 
-        private static void Dfs(List<int>[] adj, HashSet<int> visited, Stack<int> stack, int currentNode)
+        private static bool Dfs(List<int>[] adj, HashSet<int> visited, Stack<int> stack, int currentNode, bool hasCycle)
         {
-            var neighboutSize = adj[currentNode].Count;
-            for (int i = 0; i < neighboutSize; i++)
+            var neighbourSize = adj[currentNode].Count;
+            for (int i = 0; i < neighbourSize; i++)
             {
                 stack.Push(adj[currentNode][i]);
             }
 
-            while (stack.Count != 0)
+            while (stack.Count != 0 && !hasCycle)
             {
                 var neighbour = stack.Pop();
                 if (!visited.Contains(neighbour))
                 {
                     visited.Add(neighbour);
-                    Dfs(adj, visited, stack, neighbour);
+                    hasCycle = Dfs(adj, visited, stack, neighbour, hasCycle);
+                }
+                else if(neighbour != currentNode)
+                {
+                    hasCycle = true;
                 }
             }
+
+            return hasCycle;
         }
 
         private static void Solve(List<int>[] adj)
         {
             var stack = new Stack<int>();
             var visited = new HashSet<int>();
-            Dfs(adj, visited, stack, 0);
+            visited.Add(0);
+            var isTree = Dfs(adj, visited, stack, 0, false);
+            Console.WriteLine(isTree ? "NO" : "YES");
         }
     }
 }
