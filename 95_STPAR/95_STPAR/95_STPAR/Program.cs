c3407842d79e82c43fe29922c6eb129d6a04f1e4
Vincent Gim <vgim@sateva.com.au>
2017-06-28 00:20:11 +0800

    STPAR: Working solution

---
+++ 95_STPAR/95_STPAR/95_STPAR/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _95_STPAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Convert.ToInt32(Console.ReadLine());
+            while (t != 0)
+            {
+                var inputLine = Console.ReadLine();
+                var inputSplit = inputLine.Split(' ');
+
+                var q = new Queue<int>();
+                for (var i = 0; i < t; i++)
+                {
+                    q.Enqueue(Convert.ToInt32(inputSplit[i]));
+                }
+
+                var counter = 1;
+                var s = new Stack<int>();
+                while (q.Count != 0 || s.Count != 0)
+                {
+                    if ((q.Count != 0 && q.Peek() == counter) || (s.Count != 0 && s.Peek() == counter))
+                    {
+                        if (s.Count != 0 && s.Peek() == counter)
+                        {
+                            s.Pop();
+                        }
+                        else
+                        {
+                            q.Dequeue();
+                        }
+                        counter++;
+                    }
+                    else
+                    {
+                        if (q.Count != 0)
+                        {
+                            var value = q.Peek();
+                            q.Dequeue();
+
+                            if (s.Count != 0)
+                            {
+                                var sentinel = s.Peek();
+                                if (sentinel < value)
+                                {
+                                    break;
+                                }
+                            }
+                            s.Push(value);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                Console.WriteLine(s.Count == 0 && q.Count == 0 ? "yes" : "no");
+
+                t = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
