f5de37d5734a84c84a601bfba159a2032a2a4cf4
Vincent Gim <vgim@sateva.com.au>
2017-07-06 00:41:54 +0800

    TRT: Initial recursive solution

--- 740_TRT/TRT/TRT/Program.cs
+++ 740_TRT/TRT/TRT/Program.cs
@@ -10,6 +10,32 @@ namespace TRT
     {
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+
+            var input = Console.ReadLine();
+            var inputSplit = input.Split(' ');
+            var arr = new int[n];
+            for (var i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(inputSplit[i]);
+            }
+
+            var result = FindPathRecursive(arr, 0, n - 1, 1);
+
+            Console.WriteLine(result);
+        }
+
+        static int FindPathRecursive(int[] arr, int i, int j, int day)
+        {
+            if (i > j)
+            {
+                return 0;
+            }
+
+            return Math.Max(
+                FindPathRecursive(arr, i + 1, j, day + 1) + (arr[i]*day),
+                FindPathRecursive(arr, i, j - 1, day + 1) + (arr[j]*day)
+                );
         }
     }
 }
