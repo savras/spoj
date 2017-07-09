6dd45eb7f53622e0bd1d53f03961e50f09fa9da5
Vincent Gim <vgim@sateva.com.au>
2017-07-09 20:36:11 +0800

    FARIDA: Recursive solution

--- 14930_FARIDACpp/FARIDA/FARIDA/Source.cpp
+++ 14930_FARIDACpp/FARIDA/FARIDA/Source.cpp
@@ -1,3 +1,34 @@
+/*
+Test cases:
+Input:
+8
+5
+1 2 3 4 5
+1
+10
+3
+2 4 3
+0
+9
+1 3 5 9 7 10 1 10 100
+4
+4 3 2 1
+2
+5 2
+3
+2 5 2
+
+Output:
+Case 1: 9
+Case 2: 10
+Case 3: 5
+Case 4: 0
+Case 5: 122
+Case 6: 6
+Case 7: 5
+Case 8: 5
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,6 +41,27 @@ using std::cout;
 using std::max;
 using std::to_string;
 
+long long solveRecursive(const vector<int>& coins, const int current) {
+	if (current < 0) { return 0; }
+
+	return max(solveRecursive(coins, current - 2), solveRecursive(coins, current - 3)) + coins[current];
+}
+
+long long solveDp(const vector<int>& coins, const int n) {
+	vector<long long> dpArr;
+	for (size_t j = 0; j < n; j++)
+	{
+		long long iMinusTwo = j > 1 ? dpArr[j - 2] : 0;
+		long long iMinusThree = j > 2 ? dpArr[j - 3] : 0;
+
+		long long maxValue = max(iMinusTwo, iMinusThree);
+		dpArr.push_back(coins[j] + maxValue);
+	}
+
+	int arrayOffset = 1;	
+	return max(dpArr[n - arrayOffset], dpArr[n - arrayOffset - 1]);
+}
+
 int main() {
 
 	int t;
@@ -20,7 +72,7 @@ int main() {
 		int n;
 		cin >> n;
 
-		vector<int> coins;;
+		vector<int> coins;
 
 		int value;
 		for (size_t j = 0; j < n; j++)
@@ -42,17 +94,11 @@ int main() {
 		}
 
 		// Solve
-		vector<long long> dpArr;
-		for (size_t j = 0; j < n; j++)
-		{
-			long long iMinusTwo = j > 1 ? dpArr[j - 2] : 0;
-			long long iMinusThree = j > 2 ? dpArr[j - 3] : 0;
+		//long long result = solveDp(coins, n);
 
-			long long maxValue = max(iMinusTwo, iMinusThree);
-			dpArr.push_back(coins[j] + maxValue);
-		}
+		long long result = max(solveRecursive(coins, n - 1), solveRecursive(coins, n - 2));
+		cout << "Case " + to_string((i + 1)) + ": " + to_string(result) << endl;
 
-		int arrayOffset = 1;
-		cout << "Case " + to_string((i + 1)) + ": " + to_string(max(dpArr[n - arrayOffset], dpArr[n - arrayOffset - 1])) << endl;
+		coins.clear();
 	}
 }
