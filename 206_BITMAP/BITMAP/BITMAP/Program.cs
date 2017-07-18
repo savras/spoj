8d5b5b37bc6b5b088cc01c57266839213f59b6cc
Vincent Gim <vgim@sateva.com.au>
2017-07-18 21:37:24 +0800

    BITMAP: Refactored BFS solution into a function

--- 206_BITMAP/BITMAP/BITMAP/Program.cs
+++ 206_BITMAP/BITMAP/BITMAP/Program.cs
@@ -49,70 +49,78 @@ namespace BITMAP
                     }
                 }
 
-                // Solve BFS
-                foreach (var cell in onePositions)
-                {
-                    InitializeVisited(visitedArr, n, m);
-                    var queue = new Queue<Tuple<int, int>>();
-                    queue.Enqueue(new Tuple<int, int>(cell.Item1, cell.Item2));
-                    queue.Enqueue(new Tuple<int, int>(-1, -1));
-                    visitedArr[cell.Item1, cell.Item2] = true;
+                SolveBfs(onePositions, arr, visitedArr, n, m);
 
-                    var cost = 0;
-
-                    while (queue.Count > 0)
-                    {
-                        var item = queue.Dequeue();
-                        if (item.Item1 == -1 && item.Item2 == -1)
-                        {
-                            queue.Enqueue(new Tuple<int, int>(-1, -1));
-                            if (queue.Peek().Item2 == -1 && queue.Peek().Item1 == -1)
-                            {
-                                break;
-                            }
-
-                            cost++;
-                            continue;
-                        }
-
-                        var i = item.Item1;
-                        var j = item.Item2;
-                        
-                        arr[i, j] = cost;
-
-                        if (i > 0 && !visitedArr[i - 1, j] && arr[i - 1, j] != 0 && arr[i - 1, j] > cost)
-                        {
-                            queue.Enqueue(new Tuple<int, int>(i - 1, j));
-                            visitedArr[i - 1, j] = true;
-                        }
-                        if (i < n - 1 && !visitedArr[i + 1, j] && arr[i + 1, j] != 0 && arr[i + 1, j] > cost)
-                        {
-                            queue.Enqueue(new Tuple<int, int>(i + 1, j));
-                            visitedArr[i + 1, j] = true;
-                        }
-                        if (j > 0 && !visitedArr[i, j - 1] && arr[i, j - 1] != 0 && arr[i, j - 1] > cost)
-                        {
-                            queue.Enqueue(new Tuple<int, int>(i, j - 1));
-                            visitedArr[i, j - 1] = true;
-                        }
-                        if (j < m - 1 && !visitedArr[i, j + 1] && arr[i, j + 1] != 0 && arr[i, j + 1] > cost)
-                        {
-                            queue.Enqueue(new Tuple<int, int>(i, j + 1));
-                            visitedArr[i, j + 1] = true;
-                        }
-                    }
-                }
-
-                // Print result
-                for (var i = 0; i < n; i++)
-                {
-                    for (var j = 0; j < m; j++)
-                    {
-                        Console.Write(arr[i, j] + " ");
-                    }
-                    Console.WriteLine();
-                }
+                PrintResult(n, m, arr);
                 Console.ReadLine();
+            }
+        }
+
+        private static void PrintResult(int n, int m, int[,] arr)
+        {
+            for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void SolveBfs(List<Tuple<int, int>> onePositions, int[,] arr, bool[,] visitedArr, int n, int m)
+        {
+            foreach (var cell in onePositions)
+            {
+                InitializeVisited(visitedArr, n, m);
+                var queue = new Queue<Tuple<int, int>>();
+                queue.Enqueue(new Tuple<int, int>(cell.Item1, cell.Item2));
+                queue.Enqueue(new Tuple<int, int>(-1, -1));
+                visitedArr[cell.Item1, cell.Item2] = true;
+
+                var cost = 0;
+
+                while (queue.Count > 0)
+                {
+                    var item = queue.Dequeue();
+                    if (item.Item1 == -1 && item.Item2 == -1)
+                    {
+                        queue.Enqueue(new Tuple<int, int>(-1, -1));
+                        if (queue.Peek().Item2 == -1 && queue.Peek().Item1 == -1)
+                        {
+                            break;
+                        }
+
+                        cost++;
+                        continue;
+                    }
+
+                    var i = item.Item1;
+                    var j = item.Item2;
+
+                    arr[i, j] = cost;
+
+                    if (i > 0 && !visitedArr[i - 1, j] && arr[i - 1, j] != 0 && arr[i - 1, j] > cost)
+                    {
+                        queue.Enqueue(new Tuple<int, int>(i - 1, j));
+                        visitedArr[i - 1, j] = true;
+                    }
+                    if (i < n - 1 && !visitedArr[i + 1, j] && arr[i + 1, j] != 0 && arr[i + 1, j] > cost)
+                    {
+                        queue.Enqueue(new Tuple<int, int>(i + 1, j));
+                        visitedArr[i + 1, j] = true;
+                    }
+                    if (j > 0 && !visitedArr[i, j - 1] && arr[i, j - 1] != 0 && arr[i, j - 1] > cost)
+                    {
+                        queue.Enqueue(new Tuple<int, int>(i, j - 1));
+                        visitedArr[i, j - 1] = true;
+                    }
+                    if (j < m - 1 && !visitedArr[i, j + 1] && arr[i, j + 1] != 0 && arr[i, j + 1] > cost)
+                    {
+                        queue.Enqueue(new Tuple<int, int>(i, j + 1));
+                        visitedArr[i, j + 1] = true;
+                    }
+                }
             }
         }
     }
