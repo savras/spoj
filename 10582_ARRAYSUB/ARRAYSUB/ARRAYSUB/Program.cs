4084b57d98549eeeb27826cc81d137e9171ea28d
Vincent Gim <vgim@sateva.com.au>
2017-08-03 23:27:34 +0800

    ARRAYSUB: Brute force soltuion

--- 10582_ARRAYSUB/ARRAYSUB/ARRAYSUB/Program.cs
+++ 10582_ARRAYSUB/ARRAYSUB/ARRAYSUB/Program.cs
@@ -10,6 +10,23 @@ namespace ARRAYSUB
     {
         static void Main(string[] args)
         {
+            var n = int.Parse(Console.ReadLine());
+            var line = Console.ReadLine();
+            var split = line.Split(' ').ToList().Select(int.Parse).ToList();
+            var k = int.Parse(Console.ReadLine());
+
+            var max = split[0];
+            for (var i = 0; i <= n - k; i++)
+            {
+                for (var j = i; j < i + k; j++)
+                {
+                    if (max < split[j])
+                    {
+                        max = split[j];
+                    }
+                }
+                Console.WriteLine(max);
+            }
         }
     }
 }
