ae2341de93e219b27697859b761a8dd7dda42dfd
Vincent Gim <vgim@sateva.com.au>
2017-08-12 17:10:41 +0800

    FIBOSUM: Added optimized matrix exponential solution

--- 8001_FIBOSUM/FIBOSUM/FIBOSUM/Program.cs
+++ 8001_FIBOSUM/FIBOSUM/FIBOSUM/Program.cs
@@ -5,38 +5,68 @@ namespace FIBOSUM
     class Program
     {
 
-        static void MatrixExponentiation(int n, int m)
+        static void MatrixExponentiationOptimized(int n, int m)
         {
-            var F = new[,] {{1, 1}, {1, 0}};
-            var result = new[,] { { 1, 1 }, { 1, 0 } };
-
-            for (var i = 2; i < n; i++)
+            ulong sum = 0;
+            for (var i = n; i <= m; i++)
             {
-                var a = (F[0, 0] * result[0, 0]) + (F[0, 1] * result[1, 0]);
-                var b = (F[0, 0] * result[0, 1]) + (F[0, 1] * result[1, 1]);
-                var c = (F[1, 0] * result[0, 0]) + (F[1, 1] * result[1, 0]);
-                var d = (F[1, 0] * result[0, 1]) + (F[1, 1] * result[1, 1]);
+                var result = new[,] {{1, 1}, {1, 0}};
+                if (i == 0)
+                {
+                    sum += 0;
+                }
+                else
+                {
+                    GetFibonacci(result, i);
+                    sum += (ulong)result[0, 1]; // or result[1, 0];
+                }
+            }
+            Console.WriteLine(sum);
+        }
 
-                result[0, 0] = a;
-                result[0, 1] = b;
-                result[1, 0] = c;
-                result[1, 1] = d;
+        // O(log n)
+        static void GetFibonacci(int[,] result, int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return;
             }
 
-            var fibN = result[0, 0];
+            GetFibonacci(result, n/2);
+            var F = new[,] {{1, 1}, {1, 0}};
+            Multiply(result, result);
+
+            if (n%2 != 0)
+            {
+                Multiply(result, F);
+            }
+        }
+
+        static void Multiply(int[,] result, int[,] F)
+        {
+            var a = (F[0, 0]*result[0, 0]) + (F[0, 1]*result[1, 0]);
+            var b = (F[0, 0]*result[0, 1]) + (F[0, 1]*result[1, 1]);
+            var c = (F[1, 0]*result[0, 0]) + (F[1, 1]*result[1, 0]);
+            var d = (F[1, 0]*result[0, 1]) + (F[1, 1]*result[1, 1]);
+
+            result[0, 0] = a;
+            result[0, 1] = b;
+            result[1, 0] = c;
+            result[1, 1] = d;
         }
 
         static void Main(string[] args)
         {
             var t = int.Parse(Console.ReadLine());
-            var line = Console.ReadLine();
-            var split = line.Split(' ');
-            var n = int.Parse(split[0]);
-            var m = int.Parse(split[1]);
 
             for (var i = 0; i < t; i++)
             {
-                MatrixExponentiation(n, m);
+                var line = Console.ReadLine();
+                var split = line.Split(' ');
+                var n = int.Parse(split[0]);
+                var m = int.Parse(split[1]);
+
+                MatrixExponentiationOptimized(n, m);
                 // Dijkstra Fibonacci 
             }
         }
