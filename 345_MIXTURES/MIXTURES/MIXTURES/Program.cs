bda16c4349fa2d97b813919e15a9acf42628f387
Vincent Gim <vgim@sateva.com.au>
2017-08-14 22:19:13 +0800

    MIXTURES: Initial logic fix

--- 345_MIXTURES/MIXTURES/MIXTURES/Program.cs
+++ 345_MIXTURES/MIXTURES/MIXTURES/Program.cs
@@ -12,40 +12,49 @@ namespace MIXTURES
                 var n = int.Parse(Console.ReadLine());
                 var line = Console.ReadLine();
                 var split = line.Split(' ');
-                var inputArr = Array.ConvertAll(split, int.Parse);
+                var chemicals = Array.ConvertAll(split, int.Parse);
 
-                var memoSum = new int[n, n];    // Mixture
-                var memoMultiplication = new int[n, n]; // Smoke
+                var mixture = new int[n, n];
+                var smoke = new int[n, n];
 
+                for (var j = 0; j < n; j++)
+                {
+                    for (var k = j; k >= 0; k++)
+                    {
+                        mixture[j, k] = -1;
+                    }
+                }
                 // Fill memoSum and memoMultiplication diagonally.
                 for (var j = 1; j < n; j++)
                 {
                     for (var k = j - 1; k >= 0; k--)
                     {
-                        var multiplication = 0;
-                        if (memoSum[j - 1, k] == -1 || memoSum[j, k + 1] == -1)
+                        var smokeAmount = 0;
+                        if (mixture[j - 1, k] == -1 || mixture[j, k + 1] == -1)
                         {
-                            memoSum[j, k] = (inputArr[j] + inputArr[k])%100;
+                            mixture[j, k] = (chemicals[j] + chemicals[k])%100;
                         }
                         else
                         {
-                            var sumTop = (memoSum[j - 1, k] + inputArr[j])%100;
-                            var sumRight = (memoSum[j, k + 1] + inputArr[k])%100;
-                            if (sumTop > sumRight)
+                            var mixtureTop = (mixture[j - 1, k] + chemicals[j])%100;
+                            var mixtureRight = (mixture[j, k + 1] + chemicals[k])%100;
+                            if (mixtureTop + chemicals[j] <= mixtureRight + chemicals[k])
                             {
-                                memoSum[j, k] = sumTop;
-                                multiplication = inputArr[j] + memoSum[j, k];
+                                mixture[j, k] = mixtureTop + chemicals[j];
+                                smokeAmount = mixture[j, k] * chemicals[j];
                             }
                             else
                             {
-                                memoSum[j, k] = sumRight;
-                                multiplication = inputArr[k] + memoSum[j, k];
+                                mixture[j, k] = mixtureRight + chemicals[k];
+                                smokeAmount = mixture[j, k] * chemicals[k];
                             }
                         }
 
-                        memoMultiplication[j, k] = Math.Max(memoSum[j - 1, k], memoSum[j, k + 1]) + multiplication;
+                        smoke[j, k] = smokeAmount;
                     }
                 }
+
+                Console.WriteLine(smoke[0, n-1]);
             }
         }
     }
