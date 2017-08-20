76473134eb0c9eb08c27053c85f8995e917131d7
Vincent Gim <vgim@sateva.com.au>
2017-08-20 17:42:09 +0800

    ABCDEF: Initial solution

--- 4580_ABCDEF/ABCDEF/ABCDEF/Class1.cs
+++ 4580_ABCDEF/ABCDEF/ABCDEF/Class1.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Formula: ((a * b + c)/ d) - e = f
+// Can be rewritten as: a * b + c = (f + e) * d
+// Brute force, get permutation of all possible values in S for both  sides of the formula.
+// Then, for each LFS (or each RHS), count the number of values that are equal on the other side.
 
 namespace ABCDEF
 {
