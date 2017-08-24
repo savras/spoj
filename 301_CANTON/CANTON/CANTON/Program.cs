060ad979293c921e843d1d4f7124f7082f9d2b1e
Vincent Gim <vgim@sateva.com.au>
2017-08-25 00:11:33 +0800

    CANTON: Leaving this for now to be solved later

--- 301_CANTON/CANTON/CANTON/Program.cs
+++ 301_CANTON/CANTON/CANTON/Program.cs
@@ -4,13 +4,65 @@
 // Triangular number's formula, 2 * c = n + (n + 1) will give us the filled diagonal of the triangle.
 // Using the value of the triangle from the formula, we can deduct c from it.
 
+using System;
+
 namespace CANTON
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var t = int.Parse(Console.ReadLine());
 
+            for (var i = 0; i < t; i++)
+            {
+                var term = int.Parse(Console.ReadLine());
+
+                Solve(term);
+            }
+        }
+
+        private static void Solve(int term)
+        {
+            var c = term*2;
+
+            // Solve for n^2 + n = 2 * term using quadratic formula
+            var valueToSquareRoot = 1 - (4*(c * -1));
+            var squareRootResult = Math.Sqrt(valueToSquareRoot);
+            var n = Math.Ceiling(Math.Abs((1 - squareRootResult) / 2));
+
+            // Using the triangular formula, we get the largest value that we can get when we fill the triangle.
+            var largestValueForTriangle = (int)(Math.Pow(n, 2) + n)/2;
+
+            
+            // ToDo: Solve this later.
+            Console.WriteLine(largestValueForTriangle);
+
+            // Console.WriteLine("TERM {0} IS {1} / {2}", term, rowNumber, colNumber);
         }
     }
 }
+
+/*
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    long long int n , x ,i , j ,sub , sum , t;
+    
+    scanf("%lld",&t);
+    while(t--)
+    {
+        scanf("%lld",&x);
+        n=ceil((-1+sqrt(1+8*x))/2);
+        sub = x - n*(n-1)/2;
+        sum = n+1;
+        if (n%2==0)
+            printf("TERM %lld IS %lld/%lld\n",x , sub , sum-sub);    
+        else
+            printf("TERM %lld IS %lld/%lld\n",x, sum -sub ,sub);            
+    }
+    return 0;
+}
+
+ */
