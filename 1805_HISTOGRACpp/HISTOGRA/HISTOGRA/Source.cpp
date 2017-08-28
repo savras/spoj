16b4f17e3839c226a2411e9ad2b85c4b38e5b298
Vincent Gim <vgim@sateva.com.au>
2017-08-28 23:21:10 +0800

    HISTOGRA: Added comments

--- 1805_HISTOGRACpp/HISTOGRA/HISTOGRA/Source.cpp
+++ 1805_HISTOGRACpp/HISTOGRA/HISTOGRA/Source.cpp
@@ -8,10 +8,11 @@
 * When we encounter smaller heights, this is when we need to figure out the area of the rectangle at the previous
 * index.
 *
-* To calculate the area of the previous rectangle, we take the height of the previous rectangle,
+* To calculate the area of the previous rectangle, we take the height of the previous rectangle "step",
 * multiply by the start index of the previous rectangle up until the previous index. This gives us
-* area = height of previous rectangle * width of rectangle.
-* area = height.Peek() * (i - startIndex.Peek())
+* area = height of previous rectangle step * width of rectangle.
+* Note: Notice that each step is recorded as we encounter an taller rectangle. This way, we know how far back
+*		the width of the evaluated rectangle is.
 *
 * We need to do this for each item in the height stack that is larger than the height currently being assessed.
 */
