3cd1d51b016b6a89b244582d224e0f966b4635be
Vincent Gim <vgim@sateva.com.au>
2017-09-02 15:14:53 +0800

    BRCKTS: Code to build segment tree

--- 61_BRCKTS/BRCKTS/BRCKTS/Program.cs
+++ 61_BRCKTS/BRCKTS/BRCKTS/Program.cs
@@ -1,4 +1,5 @@
-﻿/* Solution: '(' represents +1, ')' represents -1. 
+﻿/* Solution: '(' represents +1.
+ *           ')' represents -1. 
  *           Use prefix array concept and check that the value at every index is positive, and 
  *           the value in the last index is 0.
  */
@@ -19,12 +20,12 @@ namespace BRCKTS
 
             // Number of leaf nodes in a full tree is 2^h, where h is height of the tree.
             // Therefore, 2^h = n. Via log identity, h = log n.
-            var height = Math.Log(n, 2);
-            var leafNodes = (int)Math.Ceiling(Math.Pow(2, height));
+            var height = (int)Math.Ceiling(Math.Log(n, 2));
+            var leafNodes = (int) Math.Pow(2, height);
             var size =  (leafNodes * 2) - 1;    // Number of leaves in level before leaf nodes in full tree is just the no. of leaf notes - 1.
             var tree = new int[size];
 
-            BuildIntervalTree(brackets, tree, 0, size - 1, 0);
+            BuildIntervalTree(brackets, tree, 0, n - 1, 0);
 
             for (var i = 0; i < m; i++)
             {
@@ -50,17 +51,20 @@ namespace BRCKTS
             
         }
 
-        static void BuildIntervalTree(string brackets, int[] tree, int low, int high, int current)
+        static int  BuildIntervalTree(string brackets, int[] tree, int low, int high, int current)
         {
             if (low >= high)
             {
-                tree[current] = brackets[current];
-                return;
+                var value = brackets[low] == ('(') ? 1 : -1;
+                tree[current] = value;
+                return value;
             }
 
             var mid = low + (high - low)/2;
-            BuildIntervalTree(brackets, tree, low, mid, (2 * current) + 2);
-            BuildIntervalTree(brackets, tree, mid + 1, high, (2 * current) + 1);
+            var leftValue = BuildIntervalTree(brackets, tree, low, mid, (2 * current) + 1);
+            var rightValue = BuildIntervalTree(brackets, tree, mid + 1, high, (2 * current) + 2);
+            tree[current] = leftValue + rightValue;
+            return tree[current];
         }
     }
 }
