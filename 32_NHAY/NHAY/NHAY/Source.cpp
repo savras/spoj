54863c8cf5c116b04650eca42ea1e86c091e26b3
Vincent Gim <vgim@sateva.com.au>
2017-09-13 00:09:12 +0800

    NHAY: Added initial outline

--- 32_NHAY/NHAY/NHAY/Source.cpp
+++ 32_NHAY/NHAY/NHAY/Source.cpp
@@ -6,12 +6,37 @@
  *		1) Deterministic Finite Automata (Example -1) - https://www.youtube.com/watch?v=40i4PKpM0cI
  */
 #include <iostream>
+#include <string>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+using std::vector;
+
+void buildDsa(string needle, vector<vector<int>>& dsa) {
+
+}
+
+void performKmp(string needle, string haystack, const vector<vector<int>>& dsa)
+{
+
+}
 
 int main() {
+	int n;
+	cin >> n;
+
+	string needle;
+	cin >> needle;
+
+	string haystack;
+	cin >> haystack;
+
+	vector<vector<int>> dsa(n);
+	buildDsa(needle, dsa);
+	performKmp(needle, haystack, dsa);
 
 	return 0;
 }
