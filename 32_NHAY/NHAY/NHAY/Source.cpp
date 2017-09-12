7efec8dcdc2042352d9a1f25b56f22a04786f0e0
Vincent Gim <vgim@sateva.com.au>
2017-09-12 19:10:27 +0800

    NHAY: Added links to tutorials

--- 32_NHAY/NHAY/NHAY/Source.cpp
+++ 32_NHAY/NHAY/NHAY/Source.cpp
@@ -1,3 +1,10 @@
+/*
+ * KMP tutorial:
+ *		1) Knuth-Morris-Pratt algorithm - https://www.youtube.com/watch?v=5i7oKodCRJo
+ *		2) KMP simplified - https://www.youtube.com/watch?v=PvAngd1J5kw
+ * DSA (Deterministic finite-state automata:
+ *		1) Deterministic Finite Automata (Example -1) - https://www.youtube.com/watch?v=40i4PKpM0cI
+ */
 #include <iostream>
 
 using std::cin;
