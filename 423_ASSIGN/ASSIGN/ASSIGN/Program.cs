20f6cc57abaf54487fb0f2cf998162b9d0e44bd1
Vincent Gim <vgim@sateva.com.au>
2017-09-21 00:23:31 +0800

    ASSIGN: Bruteforce recursive solution

--- 423_ASSIGN/ASSIGN/ASSIGN/Program.cs
+++ 423_ASSIGN/ASSIGN/ASSIGN/Program.cs
@@ -4,12 +4,74 @@
  * thus, we recurse and memoize the number of combinations for each result below the current student.
  * Extend this to DP.
  */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ASSIGN
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var t = int.Parse(Console.ReadLine());
+            for (var i = 0; i < t; i++)
+            {
+                // Track the students and the subjects that they are interested in
+                var dict = new Dictionary<int, HashSet<int>>();
+                var n = int.Parse(Console.ReadLine());
+
+                for (var j = 0; j < n; j++)
+                {
+                    var line = Console.ReadLine();
+                    var split = line.Split(' ');
+
+                    var likedSubjects = new HashSet<int>(); ;
+                    for (var k = 0; k < n; k++)
+                    {
+                        var value = int.Parse(split[k]);
+                        if (value == 1)
+                        {
+                            likedSubjects.Add(k);
+                        }
+                    }
+                    dict.Add(j, likedSubjects);
+                }
+
+                var result = SolveRecursive(dict, 0, new List<int>());
+                Console.WriteLine(result);
+            }
+        }
+
+        private static ulong SolveRecursive(Dictionary<int, HashSet<int>> dict, int current, List<int> chosen)
+        {
+            ulong sum = 0;
+
+            if (chosen.Count == dict.Count)
+            {
+                return 1;
+            }
+
+            if (current >= dict.Count)
+            {
+                return 0;
+            }
+
+            var remainingChoices = dict[current].Except(chosen).ToList();
+            if (!remainingChoices.Any())
+            {
+                return 0;
+            }
+
+            foreach (var choice in remainingChoices)
+            {
+                chosen.Add(choice);
+                sum += SolveRecursive(dict, current + 1, chosen);
+                chosen.RemoveAt(chosen.Count - 1);
+            }
+
+            return sum;
         }
     }
 }
