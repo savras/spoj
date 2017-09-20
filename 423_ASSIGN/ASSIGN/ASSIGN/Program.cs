a6df3fb04cf4b1b52cb79a2890bcb29ef8786753
Vincent Gim <vgim@sateva.com.au>
2017-09-20 22:23:42 +0800

    ASSIGN: Added solution design

--- 423_ASSIGN/ASSIGN/ASSIGN/Program.cs
+++ 423_ASSIGN/ASSIGN/ASSIGN/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace ASSIGN
+﻿/*
+ * Idea: Recursion + back tracking => DP.
+ * Not that top down backtrackking gives the same result for each student below the current,
+ * thus, we recurse and memoize the number of combinations for each result below the current student.
+ * Extend this to DP.
+ */
+namespace ASSIGN
 {
     class Program
     {
