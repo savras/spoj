4617fe3671f58d4135098f3984791b6335403297
Vincent Gim <vgim@sateva.com.au>
2017-10-03 23:40:25 +0800

    LCA: Added DFS to get tree height

--- 14932_LCA/LCA/LCA/Program.cs
+++ 14932_LCA/LCA/LCA/Program.cs
@@ -1,14 +1,14 @@
 ﻿/* First naive implementation is to use DFS and get the path taken to find both nodes,
  * then iterate through them to find the first common node.
- * 
+ * I will then extend my solution to method in here: https://www.topcoder.com/community/data-science/data-science-tutorials/range-minimum-query-and-lowest-common-ancestor/#Lowest%20Common%20Ancestor%20(LCA)
  * Other solutions:
- *      Heavy Light Decomposition
- *      Range Minimum Query (Segment Tree)
+ *      Heavy Light Decomposition/ Range Minimum Query (Segment Tree)
  *      Sparse tree.
  *  Similar questions: LCASQ, TALCA (https://www.codechef.com/problems/TALCA)
  */
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LCA
 {
@@ -36,6 +36,13 @@ namespace LCA
                     }
                 }
 
+                // Method 1 in TopCoder tutorial using square root of sections
+                // Preprocess list and build P array.
+                var p = new int[n];
+                var height = GetTreeHeight(adjList, 0, 0);
+
+                var lastLevelOfSection = (int) Math.Sqrt(height) - 1;
+                DfsBuildP(adjList, p, lastLevelOfSection, 0, 0, 0);
 
                 var q = int.Parse(Console.ReadLine());
                 for (var j = 0; j < q; j++)
@@ -46,15 +53,46 @@ namespace LCA
                     var v2 = int.Parse(querySplit[1]) - 1;
 
                     // DFS recursive
-                    var visited1 = new List<int>();
-                    Dfs(adjList, visited1, 0, v1);
-
-                    var visited2 = new List<int>();
-                    Dfs(adjList, visited2, 0, v2);
-
-                    var lca = GetLca(visited1, visited2);
-                    Console.WriteLine(lca + 1);
+                    //var visited1 = new List<int>();
+                    //Dfs(adjList, visited1, 0, v1);
+                    //
+                    //var visited2 = new List<int>();
+                    //Dfs(adjList, visited2, 0, v2);
+                    //
+                    //var lca = GetLca(visited1, visited2);
+                    //Console.WriteLine(lca + 1);
                 }
+            }
+        }
+
+        private static int GetTreeHeight(List<List<int>> adjList, int currentNode, int height)
+        {
+            if (!adjList[currentNode].Any())
+            {
+                return height;
+            }
+
+            var maxHeight = height;
+            foreach (var neighbour in adjList[currentNode])
+            {
+                maxHeight = Math.Max(maxHeight, GetTreeHeight(adjList, neighbour, height + 1));
+            }
+
+            return maxHeight;
+        }
+
+        private static void DfsBuildP(List<List<int>> adjList, int[] p, int lastLevelOfSection, int parentOfSection, int currentNode, int currentLevel)
+        {
+            p[currentNode] = parentOfSection;
+            if (lastLevelOfSection == currentLevel)
+            {
+                lastLevelOfSection = (2 * lastLevelOfSection) - 1;
+                parentOfSection = currentNode;
+            }
+
+            foreach (var neighbour in adjList[currentNode])
+            {
+                DfsBuildP(adjList, p, lastLevelOfSection, parentOfSection, neighbour, currentLevel + 1);
             }
         }
 
